29f09c30cb3583476297ac045be4f9ee14b44e46
fateiceb <993661862@qq.com>
2021-01-27 00:26:44 +0800

    fix makegood

--- stack/makeGood_1544.go
+++ stack/makeGood_1544.go
@@ -8,24 +8,26 @@ import (
 func makeGood(s string) string {
 
 	for i := 0; i < len(s)-1; i++ {
+		fmt.Println(i)
 		if s[i] == byte(unicode.ToLower(rune(s[i+1]))) || s[i] == byte(unicode.ToUpper(rune(s[i+1]))) {
+			fmt.Println(string(s[i]), string(s[i+1]))
 			if !(s[i] == s[i+1]) {
 				s = s[:i] + s[i+2:]
-				fmt.Println(s, i)
 				i = 0
 			}
 		}
-		if len(s) == 2 {
 
-		}
 	}
+	// if s[0] == byte(unicode.ToLower(rune(s[1]))) || s[0] == byte(unicode.ToUpper(rune(s[i+1]))) {
+
+	// }
 	return s
 }
 func main() {
-	s := "leEeetcode"
-	s1 := "abBAcC"
-	s2 := "s"
-	fmt.Println(makeGood(s))
-	fmt.Println(makeGood(s1))
+	// s := "leEeetcode"
+	// s1 := "abBAcC"
+	s2 := "qFxXfQo"
+	// fmt.Println(makeGood(s))
+	// fmt.Println(makeGood(s1))
 	fmt.Println(makeGood(s2))
 }
